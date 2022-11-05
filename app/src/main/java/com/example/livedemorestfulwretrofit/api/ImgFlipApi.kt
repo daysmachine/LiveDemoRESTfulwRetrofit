0ae61b340632e8a49923b0d57f30370850048517
Daisy Catalan <dcatalan@csu.fullerton.edu>
2022-11-04 17:12:47 -0700

    up to slide 76

--- app/src/main/java/com/example/livedemorestfulwretrofit/api/ImgFlipApi.kt
+++ app/src/main/java/com/example/livedemorestfulwretrofit/api/ImgFlipApi.kt
@@ -1,11 +1,19 @@
 package com.example.livedemorestfulwretrofit.api
 
 import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ImgFlipApi
 {
     // get_memes
     @GET("get_memes")
     fun fetchTemplates(): Call<ImgFlipGetMemesResponse>
+
+    @FormUrlEncoded
+    @POST("caption_image")
+    fun captionImage(
+        //TODO
+    ): Call<ImgFlipCaptionImageResponse>
 }
